60ba0ba53765d939d9ef59b9a3dd901393447de7
Vitor <vitorrf4@gmail.com>
2023-08-31 16:22:50 -0300

    cria arquivo de logica de negocio

---
+++ src/main/java/org/example/ProdutoLogic.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class ProdutoLogic {
+    ProdutoView view = new ProdutoView();
+    Database db = new Database();
+
+    public void cadastrarProduto (Produto produto) {
+
+    }
+
+    public void movimentarProduto (MovimentoProduto movimento) {
+
+    }
+
+    public void consultarProduto (int id) {
+
+    }
+}

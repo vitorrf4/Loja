93d7a1bf9731b4458478114c9f18528ede625f94
Vitor <vitorrf4@gmail.com>
2023-09-01 17:21:55 -0300

    arruma foreign key de movimentos

--- src/main/java/org/example/MovimentoProduto.java
+++ src/main/java/org/example/MovimentoProduto.java
@@ -3,7 +3,7 @@ package org.example;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "MovimentoProduto")
+@Table(name = "movimentos_produto")
 public class MovimentoProduto {
     public enum Movimentos {
         ENTRADA,
@@ -12,26 +12,66 @@ public class MovimentoProduto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "codigo_movimento", nullable = false)
+    @Column(name = "id_movimento", nullable = false)
     private int idMovimento;
 
-    @ManyToOne(targetEntity = Produto.class,fetch = FetchType.LAZY)
-    @JoinColumn(name = "id_produto", referencedColumnName = "codigo_produto", nullable = false)
-    private int produto;
+    @ManyToOne(targetEntity = Produto.class)
+    @JoinColumn(name = "id_produto", nullable = false)
+    private Produto produto;
 
     @Column(name = "quantidade_movimento", nullable = false)
     private int quantidadeMovimento;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "tipo_movimento", nullable = false)
+    @Column(name = "tipo_movimento",  nullable = false)
     private Movimentos tipoMovimento;
 
     public MovimentoProduto() {
     }
 
-    public MovimentoProduto(int codigoProduto, int quantidadeMovimento, Movimentos tipoMovimento) {
-        this.produto = codigoProduto;
+    public MovimentoProduto(Produto produto, int quantidadeMovimento, Movimentos tipoMovimento) {
+        this.produto = produto;
         this.quantidadeMovimento = quantidadeMovimento;
         this.tipoMovimento = tipoMovimento;
+    }
+
+    public int getIdMovimento() {
+        return idMovimento;
+    }
+
+    public void setIdMovimento(int idMovimento) {
+        this.idMovimento = idMovimento;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public int getQuantidadeMovimento() {
+        return quantidadeMovimento;
+    }
+
+    public void setQuantidadeMovimento(int quantidadeMovimento) {
+        this.quantidadeMovimento = quantidadeMovimento;
+    }
+
+    public Movimentos getTipoMovimento() {
+        return tipoMovimento;
+    }
+
+    public void setTipoMovimento(Movimentos tipoMovimento) {
+        this.tipoMovimento = tipoMovimento;
+    }
+
+    @Override
+    public String toString() {
+        return "ID Movimento: " + idMovimento +
+                " | Produto: " + produto.getIdProduto() +
+                " | Quantidade: " + quantidadeMovimento +
+                " | Tipo: " + tipoMovimento;
     }
 }
